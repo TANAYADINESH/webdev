5c46f3642a12c3b7c1974ae85803885fc5d3cd95
Tanaya Dinesh <tanayadhondre@gmail.com>
2026-08-26 19:02:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Hello, World!");
+
+let a = 'Tanaya'
+const b = 'Dinesh'
+
+let shoppingList = ['milk', 'bread', 'eggs', 'cheese']
+let num = [1,2,3,4,5,6,7,8,9,10]
+
+let obj = {"Hindavi": 20, "Tanaya": 21}
+let grups = [
+    {"student1_name": "Hindavi" ,"student2_name": "Tanaya"},
+    {"student1_name": "Anushka","student2_name": "Shreya"}
+]
+function greeting(a){
+    console.log(`Hello ${a}`)
+}
+greeting(a)
+
+console.log(a)
+console.log(b)
+console.log(shoppingList)
+console.log(num)
+console.log(obj)
+console.log(grups)
